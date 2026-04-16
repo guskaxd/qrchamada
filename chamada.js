4b69fd71d38417f6f3d61956deaf79eebff83755
guskaxd <guskovcs@gmail.com>
2026-04-15 22:03:58 -0300

    presença1.9

--- chamada.js
+++ chamada.js
@@ -185,7 +185,7 @@ app.get('/', (req, res) => {
         <body>
             <div class="card">
                 <div class="header">
-                    <img src="/logo-ifma.png" alt="Logo IFMA" class="logo-ifma">
+                    <img src="/ifma.jpg" alt="Logo IFMA" class="logo-ifma">
                     
                     <h1>Chamada Digital</h1>
                     <p>Configure a disciplina e a data para gerar o código de presença da turma.</p>
@@ -194,7 +194,7 @@ app.get('/', (req, res) => {
                 <form action="/qr-turma" method="GET">
                     <div class="input-group">
                         <label for="disciplina">Disciplina</label>
-                        <input type="text" id="disciplina" name="disciplina" placeholder="Ex: Web II, Informática 2..." required autocomplete="off">
+                        <input type="text" id="disciplina" name="disciplina" placeholder="Ex: Web II, Estrutura de Dados..." required autocomplete="off">
                     </div>
                     
                     <div class="input-group">
@@ -203,7 +203,7 @@ app.get('/', (req, res) => {
                     </div>
                     
                     <button type="submit" class="btn">
-                        <span>📱</span> Gerar QR Code da Turma
+                        <span></span> Gerar QR Code da Turma
                     </button>
                 </form>
 
